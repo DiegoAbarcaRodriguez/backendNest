96582fba07e80259ab96c3177ab4a21281ce42fe
Diego Abarca <abarcarodriguezdiego@gmail.com>
2023-06-19 22:11:42 -0600

    primer commit

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    })
+  );
+
+  await app.listen(process.env.PORT ?? 3000); //process.env.PORT  se define automaticamente en el servicio a la nube en donde se desplega el aplicativo
+}
+bootstrap();
